f46629c8c6657d87697159dabc40a807f872c665
Albert Cedeno <albertcdn@gmail.com>
2020-09-28 16:31:35 -0400

    finished print_board method

--- working.py
+++ working.py
@@ -16,9 +16,18 @@ def print_board(bo):
     for i in range(len(bo)):
         # every 3 lines it will add dashes for readability
         if i % 3 == 0 and i != 0:
-            print("-------------")
+            print("------------------------")
 
-        for j in len(bo[0]):
+        for j in range(len(bo[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+print_board(board)
+
+
 
