d74b3f3d607cc849cdf5b0cc03980e775e764bd2
Albert Cedeno <albertcdn@gmail.com>
2020-10-05 15:08:29 -0400

    finished validation function

--- working.py
+++ working.py
@@ -11,6 +11,9 @@ board = [
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
 
+def solve(bo):
+
+
 def valid(bo, num, pos):
 
     # Check row
@@ -22,6 +25,17 @@ def valid(bo, num, pos):
     for i in range(len(bo[0])):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3+1):
+        for j in range(box_x * 3, box_x * 3 + 1):
+            if b[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
 
 
 
